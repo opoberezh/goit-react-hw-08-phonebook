4d5892feb37cdd048559fc26fda57c0e16aae651
opoberezh <124092276+opoberezh@users.noreply.github.com>
2023-10-02 11:09:29 +0000

    Deploying to gh-pages from @ opoberezh/goit-react-hw-08-phonebook@d640ba2ff776b3afdb4d1d7549ac663cee341eb1 🚀

---
+++ static/js/593.af98546f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[593],{4593:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(184),i={container:{minHeight:"cala(100vh - 50px)",display:"flex",alignItems:"center",justyfyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function o(){return(0,r.jsx)("div",{style:i.container,children:(0,r.jsx)("h1",{style:i.title,children:"Enjoy Your Phonebook App"})})}}}]);
+//# sourceMappingURL=593.af98546f.chunk.js.map
