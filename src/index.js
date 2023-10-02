01f33b14ccc9de8ff10dbc68bfde80b6f2a79c5c
opoberezh <opoberezh.2015@gmail.com>
2023-10-02 17:10:50 +0200

    up

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize';
 
-const basename = '/contacts';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={basename}>
+        <BrowserRouter>
          <App /> 
         </BrowserRouter>
       </PersistGate>
