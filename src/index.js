c7e2fd58c96bc72db2ac8727d1983635d3c3aa2c
opoberezh <opoberezh.2015@gmail.com>
2023-10-02 16:50:47 +0200

    base

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      
+        <BrowserRouter basename={basename}>
          <App /> 
-        
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   // </React.StrictMode>
