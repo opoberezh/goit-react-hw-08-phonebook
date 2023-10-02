d978c9e45115adaac4607017d9d734e5d8a65a42
opoberezh <opoberezh.2015@gmail.com>
2023-10-02 16:52:44 +0200

    app

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize';
 
-
+const basename = '/myapp';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Provider store={store}>
